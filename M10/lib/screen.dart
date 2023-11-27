34e04087bbb67355cb51af5fbf0fd11bfd33dfc8
Raven <114080089+Kyovens@users.noreply.github.com>
2023-11-27 14:21:43 +0700

    screen.dart

--- M10/lib/screen.dart
+++ M10/lib/screen.dart
@@ -1,89 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:m10/camera.dart';
 import 'package:m10/contact.dart';
-import 'package:m10/photo_screen.dart'; // Import your PhotoScreen file
 import 'package:permission_handler/permission_handler.dart';
 
 class Screen extends StatefulWidget {
-  const Screen({Key? key});
+  const Screen({super.key});
 
   @override
   State<Screen> createState() => _ScreenState();
 }
 
 class _ScreenState extends State<Screen> {
-  int noButtonCount = 0;
 
   void contact() async {
     if (await Permission.contacts.status.isGranted) {
       Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactScreen()));
-    } else {
-      if (noButtonCount < 3) {
-        // Show confirmation dialog
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text("Permission Required"),
-            content: Text("Do you want to grant contacts permission?"),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  requestContactsPermission();
-                },
-                child: Text("Yes"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  handleNoButton();
-                },
-                child: Text("No"),
-              ),
-            ],
-          ),
-        );
-      } else {
-        // Show dialog and prompt user to go to settings
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text("Permission Required"),
-            content: Text("To use this feature, you need to grant contacts permission. Go to settings?"),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  openAppSettings();
-                },
-                child: Text("Yes"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("No"),
-              ),
-            ],
-          ),
-        );
+    }
+    else {
+      var status = await Permission.contacts.request();
+      print(status);
+      if(status == PermissionStatus.granted) {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactScreen()));
+      }
+      else if (status == PermissionStatus.permanentlyDenied) {
+        openAppSettings();
       }
     }
   }
 
-  void requestContactsPermission() async {
-    var status = await Permission.contacts.request();
-    print(status);
-    if (status == PermissionStatus.granted) {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactScreen()));
-    } else if (status == PermissionStatus.permanentlyDenied) {
-      openAppSettings();
+  void camera() async{
+    if (await Permission.camera.status.isGranted) {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => CameraScreen()));
     }
-  }
-
-  void handleNoButton() {
-    setState(() {
-      noButtonCount++;
-    });
+    else {
+      var status = await Permission.camera.request();
+      print(status);
+      if (status == PermissionStatus.granted) {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => CameraScreen()));
+      }
+      else if (status == PermissionStatus.permanentlyDenied) {
+        openAppSettings();
+      }
+    }
   }
 
   @override
@@ -96,8 +54,8 @@ class _ScreenState extends State<Screen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () => contact(), child: Text("Contact")),
-            ElevatedButton(onPressed: () => photos(), child: Text("Photos")),
+            ElevatedButton(onPressed: () {contact();}, child: Text("Contact")),
+            ElevatedButton(onPressed: () {camera();}, child: Text("Camera"))
           ],
         ),
       ),
