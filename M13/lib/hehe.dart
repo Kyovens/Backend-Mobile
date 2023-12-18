a4a218571d02d554c5b15db434a534ed1d3516c1
Raven <114080089+Kyovens@users.noreply.github.com>
2023-12-18 14:24:52 +0700

    hehe.dart

--- M13/lib/hehe.dart
+++ M13/lib/hehe.dart
@@ -5,6 +5,8 @@ import 'managers/ads_manager.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final adsManager = context.watch<AdsManager>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Your App Title"),
@@ -13,15 +15,33 @@ class MainScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Your existing widgets
-
+            if (adsManager.showInterstitialAds)
+              ElevatedButton(
+                onPressed: () {
+                  _showInterstitialBeforeNavigate(context);
+                },
+                child: Text("Go to Home with Ads"),
+              ),
+            if (adsManager.showBannerAd)
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  // Your banner ad widget
+                  // Replace this with your actual banner ad widget
+                  width: 320.0,
+                  height: 50.0,
+                  color: Colors.blueGrey,
+                  child: Center(
+                    child: Text('Banner Ad'),
+                  ),
+                ),
+              ),
             Padding(
               padding: EdgeInsets.all(8.0),
               child: ElevatedButton(
                 onPressed: () {
-                  // Assuming 1000 rupiah payment to hide ads
-                  bool paymentSuccessful =
-                      context.read<AdsManager>().makePayment(1000);
+                  // Assuming 1000 rupiah payment to remove ads
+                  bool paymentSuccessful = adsManager.makePayment(1000);
 
                   if (paymentSuccessful) {
                     ScaffoldMessenger.of(context).showSnackBar(
@@ -42,6 +62,43 @@ class MainScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _showInterstitialBeforeNavigate(BuildContext context) {
+    final adsManager = context.read<AdsManager>();
+
+    // Load and show interstitial ad
+    // ... (Your interstitial ad loading and showing logic)
+
+    // When the interstitial ad is dismissed, navigate to HomeScreen
+    // Set the callback to null to avoid unwanted navigation on subsequent dismissals
+    interstitialAd.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (ad) {
+        // Navigate to the HomeScreen after the interstitial ad is dismissed
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+        // Set the callback to null to avoid unwanted navigation on subsequent dismissals
+        interstitialAd.fullScreenContentCallback = null;
+      },
+    );
+
+    interstitialAd.show();
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Center(
+        child: Text("This is the home screen."),
       ),
     );
   }
